b6d5d3d049db599b8cbc2f709a50b2e7f11af858
Pubayan Kapat <pubayankapat@gmail.com>
2025-05-26 16:38:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Java Switch Case statement/java-switch-case-statement.java
@@ -0,0 +1,11 @@
+// User function Template for Java
+
+class Solution {
+    static double switchCase(int choice, List<Double> arr) {
+        switch(choice){
+            case 1: return Math.PI*arr.get(0)*arr.get(0);
+            case 2: return arr.get(0)*arr.get(1);
+        }
+        return -1;
+    }
+}
